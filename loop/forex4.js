d3f46b9c05c8ef1ca6f2c2354b2a5f86e6a82440
MARIA EDUARDA GUSMAO PASSOS <34895571823@info.local>
2024-05-14 08:39:49 -0300

    Feat:ex4loop

---
+++ loop/forex4.js
@@ -0,0 +1,7 @@
+// Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+
+for (let num=0; num < 50; num+=3){
+    console.log("Segue tabuada do 3 até o número 10: " + num);
+}
