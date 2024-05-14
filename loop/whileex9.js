9800ac037aeefd3189f9441e106d6142b926ec7e
MARIA EDUARDA GUSMAO PASSOS <34895571823@info.local>
2024-05-14 10:52:30 -0300

    Feat:ex9loop

---
+++ loop/whileex9.js
@@ -0,0 +1,10 @@
+// Faça um programa que receba um número do usuário e mostre
+// // no console a contagem regressiva até 0.
+
+
+let num = parseInt(prompt("Ola, digite um numero: "));
+
+    
+    for(let cont = num; cont >=0; cont--){
+            console.log(cont);
+        };
