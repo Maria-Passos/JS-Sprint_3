a387acbbcf82bd49bd834865acfd2aefc421dee3
MARIA EDUARDA GUSMAO PASSOS <34895571823@info.local>
2024-05-14 10:57:56 -0300

    Feat:ex10loop

---
+++ loop/whileex10.js
@@ -0,0 +1,11 @@
+// Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
+
+ let num = parseInt(prompt("Ola, digite um numero: "));
+
+
+ while(num > 0){
+    num =  parseInt(prompt("Ola, digite um numero: "));
+    console.log( num);
+    }
+
